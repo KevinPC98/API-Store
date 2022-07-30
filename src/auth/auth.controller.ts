cb51601c5dc49d408677f92c38d9eed294b580ab
Kevin Paredes Caman <kevin.paredescaman20@gmail.com>
2022-07-29 21:37:50 -0500

    feat: create user functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/request/sign-up.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Get()
-  getAnything() {
+  async getAnything() {
     return this.authService.getAnything();
+  }
+
+  @Post('/signup')
+  async signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.createUser(signUpDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { SignUpDto } from './dto/request/sign-up.dto';
+import { hashSync } from 'bcryptjs';
+import { plainToInstance } from 'class-transformer';
+import { UserDto } from './dto/response/user.dto';
+import { Prisma, User } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  getAnything() {
-    return this.prisma.role.findFirst({});
+  async createUser(signUpDto: SignUpDto) {
+    const role = await this.prisma.role.findFirst({
+      where: {
+        name: 'CLIENT',
+      },
+      select: {
+        uuid: true,
+        name: true,
+      },
+    });
+
+    const { password } = signUpDto;
+    signUpDto.password = hashSync(password, 10);
+    const user = await this.prisma.user.create({
+      data: {
+        ...signUpDto,
+      },
+      role: {
+        connect: {
+          uuid: role.uuid,
+        },
+      },
+    });
+
+    const userDto: UserDto = signUpDto;
+    console.log(userDto);
+
+    //return plainToInstance(UserDto, {});
   }
 }
