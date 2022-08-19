3f6d4bf49429084e937db2a0a732de3036ee9029
Kevin Paredes Caman <kevin.paredescaman20@gmail.com>
2022-08-19 18:48:07 -0500

    feat: created user service functionalities

---
+++ src/users/users.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { hashSync } from 'bcryptjs';
+import { plainToInstance } from 'class-transformer';
+import { CreateUserDto } from 'src/auth/dto/request/create-user.dto';
+import { UserDto } from 'src/auth/dto/response/user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+  async findOneByEmail(email: string): Promise<UserDto> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    return plainToInstance(UserDto, user);
+  }
+
+  async createUser(input: CreateUserDto, roleUuid: string): Promise<UserDto> {
+    const { password, ...data } = input;
+
+    const user = await this.prisma.user.create({
+      data: {
+        ...data,
+        password: hashSync(password, 10),
+        role: {
+          connect: {
+            uuid: roleUuid,
+          },
+        },
+      },
+    });
+
+    return plainToInstance(UserDto, user);
+  }
+}
